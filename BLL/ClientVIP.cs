660e3dab8349f2db5d30178a3f31b00ba1292548
Makrem Taieb <makrem16790@gmail.com>
2024-11-28 10:23:34 +0100

    first commit

---
+++ BLL/ClientVIP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ClientVIP : Client
+    {
+        public decimal Ristourne { get; set; }
+        public int QuantiteRistourne { get; set; }
+
+        public ClientVIP()
+        {
+                
+        }
+        public ClientVIP(int idClient, decimal ca, DateTime dateCreation, decimal ristourne, int quantiteRistourne)
+            : base(idClient, ca, dateCreation)
+        {
+            Ristourne = ristourne;
+            QuantiteRistourne = quantiteRistourne;
+        }
+    }
+}
--- GestionClient/GestionClient.Designer.cs
+++ GestionClient/GestionClient.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            radioButton1 = new RadioButton();
+            listBox1 = new ListBox();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            radioButton2 = new RadioButton();
+            flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.LightSteelBlue;
+            label1.Location = new Point(12, 4);
+            label1.Name = "label1";
+            label1.Size = new Size(776, 52);
+            label1.TabIndex = 0;
+            label1.Text = "Gestion Clients";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(radioButton1);
+            flowLayoutPanel1.Controls.Add(radioButton2);
+            flowLayoutPanel1.Location = new Point(3, 84);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(371, 52);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(3, 3);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(132, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Client Standard";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(3, 142);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(371, 244);
+            listBox1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(button1);
+            flowLayoutPanel2.Controls.Add(button2);
+            flowLayoutPanel2.Location = new Point(3, 392);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(371, 52);
+            flowLayoutPanel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Ajouter";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(103, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Supprimer";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(141, 3);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(93, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Client VIP";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // GestionClient
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(label1);
+            Name = "GestionClient";
+            Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            flowLayoutPanel2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private RadioButton radioButton1;
+        private ListBox listBox1;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Button button1;
+        private Button button2;
+        private RadioButton radioButton2;
     }
 }
